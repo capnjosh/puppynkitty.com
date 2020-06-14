b228cfd2fd7f17f7c18fe3a4e92cecfa0062f0f9
Joshua Tan <capnjt@gmail.com>
2020-06-14 00:53:00 -0700

    Add favicon, background picker

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
+    Modernizr.on('webp', function (featureIsAvailabe) {
+        var $background = document.getElementById('background');
+        if (featureIsAvailabe) {
+            $background.style.backgroundImage = "url('" + $background.getAttribute('data-preferred') + "')";
+        }
+        else {
+            $background.style.backgroundImage = "url('" + $background.getAttribute('data-alternate') + "')";
+        }
+    });
 });
 //# sourceMappingURL=main.js.map
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,2 +1,15 @@
 document.addEventListener( 'DOMContentLoaded', () => {
+
+	Modernizr.on( 'webp', ( featureIsAvailabe ) => {
+
+		let $background = document.getElementById( 'background' )
+
+		if ( featureIsAvailabe ) {
+			$background.style.backgroundImage = `url('${ $background.getAttribute( 'data-preferred' ) }')`
+		} else {
+			$background.style.backgroundImage = `url('${ $background.getAttribute( 'data-alternate' ) }')`
+		}
+
+	} )
+
 } )
